03344d98860e5e2ccf6b7915037ed5f7ca7e12a5
Luc Dorval <ldorval@gmail.com>
2021-12-07 08:41:23 -0500

    Day 7

---
+++ Day07/Day07.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2021.Day07
+{
+    public class Day07
+    {
+        public static int SolvePart1(string input)
+        {
+            int FuelCost(int positionA, int positionB) => Math.Abs(positionA - positionB);
+            return Solve(input, FuelCost);
+        }
+        
+        public static int SolvePart2(string input)
+        {
+            int FuelCost(int positionA, int positionB) => Convert.ToInt32(Math.Abs(positionA - positionB) / 2.0 * (Math.Abs(positionA - positionB) + 1));
+            return Solve(input, FuelCost);
+        }
+
+        private static int Solve(string input, Func<int, int, int> fuelCost)
+        {
+            var positions = input.ToInts();
+            var costs = Enumerable.Range(0, positions.Max() + 1)
+                .Select(x => positions
+                    .Select(x1 => fuelCost(x, x1))
+                    .Sum());
+
+            return costs.Min();
+        }
+    }
+}
---
+++ Day07/Day07Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2021.Day07
+{
+    public class Day07Test
+    {
+        private string input = "16,1,2,0,4,2,7,1,2,14";
+        
+        [Test]
+        public void ExamplePart1()
+        {
+            Day07.SolvePart1(input).Should().Be(37);
+        }
+
+        [Test]
+        public void SolvePart1()
+        {
+            Console.Write(Day07.SolvePart1("Day07.txt".ReadAll()));
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            Day07.SolvePart2(input).Should().Be(168);
+        }
+
+        [Test]
+        public void SolvePart2()
+        {
+            Console.Write(Day07.SolvePart2("Day07.txt".ReadAll()));
+        }
+    }
+}
