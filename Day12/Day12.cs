361b0cd0c7751e4f7ce918ff398e0909f0c4c7d3
Luc Dorval <ldorval@gmail.com>
2021-12-12 16:23:09 -0500

    Day 12

---
+++ Day12/Day12.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2021.Day12
+{
+    public class Day12
+    {
+        private List<List<string>> paths = new();
+        private List<(string A, string B)> connections;
+        private bool CanVisitOneSmallCaveTwice;
+
+        public int SolvePart1(string input)
+        {
+            return Solve(input);
+        }
+
+        public int SolvePart2(string input)
+        {
+            CanVisitOneSmallCaveTwice = true;
+            return Solve(input);
+        }
+
+        private int Solve(string input)
+        {
+            connections = input.LinesToString()
+                .Select(x => (A: x.Split("-")[0], B: x.Split("-")[1]))
+                .ToList();
+
+            FindPaths(new List<string> {"start"});
+            return paths.Count;
+        }
+
+        private void FindPaths(List<string> currentPath)
+        {
+            var currentCave = currentPath.Last();
+            connections
+                .Where(x => x.A == currentCave || x.B == currentCave)
+                .Select(x => x.A == currentCave ? x.B : x.A)
+                .Where(x => x != "start")
+                .Where(x => CanVisitCave(currentPath, x))
+                .Select(x => new List<string>(currentPath) {x})
+                .ToList()
+                .ForEach(x =>
+                {
+                    if (x.Last() == "end")
+                        paths.Add(x);
+                    else 
+                        FindPaths(x);
+                });
+        }
+
+        private bool CanVisitCave(List<string> currentPath, string possibility)
+        {
+            if (possibility == "end") return true;
+            if (possibility == possibility.ToUpper()) return true;
+            var wasAlreadyVisited = currentPath.Contains(possibility);
+            
+            if (CanVisitOneSmallCaveTwice)
+            {
+                return !wasAlreadyVisited || currentPath
+                    .Where(x => x == x.ToLower())
+                    .GroupBy(x => x)
+                    .All(x => x.Count() == 1);
+            }
+            
+            return possibility == possibility.ToLower() && !wasAlreadyVisited;
+        }
+    }
+}
---
+++ Day12/Day12Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2021.Day12
+{
+    public class Day12Test
+    {
+        [Test]
+        public void Example1Part1()
+        {
+            new Day12().SolvePart1("Day12Example1.txt".ReadAll()).Should().Be(10);
+        }
+        
+        [Test]
+        public void Example2Part1()
+        {
+            new Day12().SolvePart1("Day12Example2.txt".ReadAll()).Should().Be(19);
+        }
+        
+        [Test]
+        public void Example3Part1()
+        {
+            new Day12().SolvePart1("Day12Example3.txt".ReadAll()).Should().Be(226);
+        }
+
+        [Test]
+        public void SolvePart1()
+        {
+            Console.Write(new Day12().SolvePart1("Day12.txt".ReadAll()));
+        }
+
+        [Test]
+        public void Example1Part2()
+        {
+            new Day12().SolvePart2("Day12Example1.txt".ReadAll()).Should().Be(36);
+        }
+        
+        [Test]
+        public void Example2Part2()
+        {
+            new Day12().SolvePart2("Day12Example2.txt".ReadAll()).Should().Be(103);
+        }
+        
+        [Test]
+        public void Example3Part2()
+        {
+            new Day12().SolvePart2("Day12Example3.txt".ReadAll()).Should().Be(3509);
+        }
+        
+        [Test]
+        public void SolvePart2()
+        {
+            Console.Write(new Day12().SolvePart2("Day12.txt".ReadAll()));
+        }
+    }
+}
