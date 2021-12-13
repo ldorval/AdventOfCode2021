80571199f05ab5c7c354a1d65e36832484f80cd2
Luc Dorval <ldorval@gmail.com>
2021-12-13 13:01:28 -0500

    Day 13

---
+++ Day13/Day13.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace AdventOfCode2021.Day13
+{
+    public class Day13
+    {
+        private List<Point> dots;
+        private List<(string axis, int position)> instructions;
+
+        public Day13(string input)
+        {
+            dots = input.Split("\r\n\r\n")[0].Split("\r\n")
+                .Select(x => new Point(int.Parse(x.Split(",")[0]), int.Parse(x.Split(",")[1])))
+                .ToList();
+
+            instructions = input.Split("\r\n\r\n")[1].Split("\r\n")
+                .Select(x => x.Replace("fold along ", ""))
+                .Select(x => (axis: x.Split("=")[0], position: int.Parse(x.Split("=")[1])))
+                .ToList();
+        }
+
+        public int SolvePart1()
+        {
+            Fold(instructions.First());
+            return dots.Distinct().Count();
+        }
+
+        public void SolvePart2()
+        {
+            instructions.ForEach(Fold);
+            for (var y = 0; y <= dots.Max(p => p.Y); y++)
+            {
+                var line = "";
+                for (var x = 0; x <= dots.Max(p => p.X); x++)
+                    line += dots.Any(p => p.X == x && p.Y == y) ? "#" : " ";
+
+                Console.WriteLine(line);
+            }
+        }
+
+        private void Fold((string axis, int position) instruction)
+        {
+            if (instruction.axis == "y")
+            {
+                var maxY = dots.Max(x => x.Y);
+                dots = dots
+                    .Where(p => p.Y > instruction.position)
+                    .Select(p => new Point(p.X, maxY - p.Y))
+                    .Concat(dots.Where(p => p.Y < instruction.position))
+                    .ToList();
+            }
+            else
+            {
+                var maxX = dots.Max(x => x.X);
+                dots = dots
+                    .Where(p => p.X > instruction.position)
+                    .Select(p => new Point(maxX - p.X, p.Y))
+                    .Concat(dots.Where(p => p.X < instruction.position))
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ Day13/Day13Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AdventOfCode2021.Day13
+{
+    public class Day13Test
+    {
+        [Test]
+        public void Example()
+        {
+            new Day13("Day13Example.txt".ReadAll()).SolvePart1().Should().Be(17);
+        }
+
+        [Test]
+        public void SolvePart1()
+        {
+            Console.Write(new Day13("Day13.txt".ReadAll()).SolvePart1());
+        }
+        
+        [Test]
+        public void SolvePart2()
+        {
+            new Day13("Day13.txt".ReadAll()).SolvePart2();
+        }
+    }
+}
